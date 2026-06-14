e09d072576ffc575c0365c4d09c5d002b82032ea
Nehachoubey23 <60461089+Nehachoubey23@users.noreply.github.com>
2026-06-14 19:47:57 +0530

    Added ecommerce user module with JPA and H2 setup

---
+++ website/src/main/java/ecommerce/controller/UserController.java
@@ -0,0 +1,48 @@
+package ecommerce.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ecommerce.model.User;
+import ecommerce.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/api/users")
+	public ResponseEntity<List<User>> getAllUsers() {
+		return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+	}
+
+	@PostMapping("/api/users/create")
+	public ResponseEntity<String> createUsers(@RequestBody User user) {
+		userService.create(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body("User added sucessFully.");
+	}
+
+	@GetMapping("/api/users/{id}")
+	public Optional<User> checkUser(@PathVariable Long id) {
+		return userService.fetchUserById(id);
+	}
+
+	@PutMapping("/api/users/{id}")
+	public ResponseEntity<String> editUser(@PathVariable Long id, @RequestBody User updatedUser) {
+		boolean updated = userService.editUserById(id, updatedUser);
+		if (updated)
+			return ResponseEntity.status(HttpStatus.OK).body("User updated sucessFully.");
+		return ResponseEntity.notFound().build();
+	}
+}
---
+++ website/src/main/java/ecommerce/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import ecommerce.model.User;
+import ecommerce.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+	//private List<User> userList = new ArrayList<>();
+	private final UserRepository userRepository;
+
+	@Override
+	public List<User> fetchAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public void create(User user) {
+		
+		userRepository.save(user);
+	}
+
+	@Override
+	public Optional<User> fetchUserById(Long id) {
+		// TODO Auto-generated method stub
+		userRepository.findById(id);
+		return userRepository.findById(id);
+	
+		/*
+		 * return userList.stream().filter(user ->
+		 * user.getId().equals(id)).findFirst().map(ResponseEntity::ok)
+		 * .orElse(ResponseEntity.notFound().build());
+		 */
+	}
+
+	@Override
+	public boolean editUserById(Long id, User updatedUser) {
+		// TODO Auto-generated method stub
+		return userRepository.findById(id) .map(existingUser -> {
+			existingUser.setFirstName(updatedUser.getFirstName());
+			existingUser.setLastName(updatedUser.getLastName());
+			userRepository.save(existingUser);
+			return true;
+
+		}).orElse(false);
+	}
+}
